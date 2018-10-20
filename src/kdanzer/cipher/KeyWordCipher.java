2cc3a421e232dc0d1fc55612e9e845c00d66b14b
KaNaDaLP <kdanzer@student.tgm.ac.at>
2018-10-20 22:12:40 +0200

    V3 KeyWordCipher implemented

--- src/kdanzer/cipher/KeyWordCipher.java
+++ src/kdanzer/cipher/KeyWordCipher.java
@@ -11,8 +11,29 @@ public class KeyWordCipher extends MonoAlphabeticCipher{
 		}
 		
 		public void createSecretAlphabet(String key) {
-			//Makes a secrete Alphabet from the key
-			
+			String hilfe = "";
+			for (int i = 0; i < key.length(); i ++) {
+				String str = key.charAt(i) + "";
+				if (str.matches("[a-zA-ZöäüÖÄÜß")) {
+					hilfe += str;
+				}
+			}
+			key = hilfe;
+			hilfe = "";
+			boolean letters[] = new boolean[MonoAlphabeticCipher.alphabet.length()];
+			for (int i = 0; i < key.length(); i ++) {
+				if (letters[alphabet.indexOf(key.charAt(i))] == false) {
+					hilfe += key.charAt(i);
+					letters[alphabet.indexOf(key.charAt(i))] = true;
+				}
+			}
+			key = hilfe;
+			for (int i = 0; i < key.length(); i ++) {
+				if (key.contains("" + alphabet.charAt(i))) {
+				} else {
+					key += alphabet.charAt(i);
+				}
+			}
 		}
 
 		public boolean setSecretAlphabet() {
